340a721860336b226a53b2e4044b9ff87f65519a
Genaro Adrian <49876222+genaroadrian@users.noreply.github.com>
2026-05-10 04:04:53 -0600

    ci: add ci

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const isMfe = mode === "mfe";
 
   return {
-    base: isMfe ? env.VITE_BASE_PATH || "/" : "/",
+    base: env.VITE_BASE_PATH || "/",
     build: {
       target: "esnext",
       ...(isMfe
