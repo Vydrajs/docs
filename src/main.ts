667fe296144c61b5851bde7528d824252dd1ae0b
Genaro Adrian <49876222+genaroadrian@users.noreply.github.com>
2026-05-10 04:14:55 -0600

    feat: update basepath

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ import { RootService } from "./app/services/root.bus";
 import { Inject } from "@vydra-js/core";
 import { i18n } from "@vydra-js/i18n";
 import { setBasePath } from "@shoelace-style/shoelace";
+import { appConfig } from "./app/config/app.config";
 
 setBasePath("/node_modules/@shoelace-style/shoelace/dist");
+i18n.setBasePath(appConfig.localesPath);
 
 const rootService = Inject(RootService);
 rootService.setConfig({ lang: "en" });
@@ -23,7 +25,7 @@ const outlet = document.createElement(VydraOutlet.is) as any;
 mountPoint.appendChild(outlet);
 
 const router = new VydraRouter({
-  basePath: "/",
+  basePath: appConfig.basePath,
   mountPoint,
   outlet,
   routes,
